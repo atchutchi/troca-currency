066251c11a769b648d8476239ebbca15757d6aa8
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-06-21 04:41:01 +0000

    Add comments to the function

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import requests
 
 
 def get_all_exchange_rates(base_currency):
+    """
+    Defines a get exchange rate function, creates a URL to the exchange rate API, 
+    and checks if the HTTP response status code is 200 and
+    returns a dictionary of currency codes and corresponding rates.
+    """
     url = f'https://api.exchangerate.host/latest?base={base_currency}'
 
     response = requests.get(url)
