0740403d990193ee03c5d41f5b802b6184fb4a63
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-06-21 04:55:39 +0000

    Implemented error handling using a try-except block to handle potential exceptions during the API request process

--- run.py
+++ run.py
@@ -3,16 +3,16 @@ import requests
 
 def get_all_exchange_rates(base_currency):
     """
-    Defines a get exchange rate function, creates a URL to the exchange rate API, 
-    and checks if the HTTP response status code is 200 and
+    Fetches the latest exchange rates for the given base currency and
     returns a dictionary of currency codes and corresponding rates.
     """
     url = f'https://api.exchangerate.host/latest?base={base_currency}'
 
-    response = requests.get(url)
-
-    if response.status_code == 200:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
         data = response.json()
         return data['rates']
-    else:
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching exchange rates: {e}")
         return None
