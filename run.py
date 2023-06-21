d188a6f419b59f3ac13835f73892410c017f5bc2
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-06-21 04:27:44 +0000

    Add function to fetch exchange rates for a given base currency from ExchangeRate API

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
 import requests
+
+
+def get_all_exchange_rates(base_currency):
+    url = f'https://api.exchangerate.host/latest?base={base_currency}'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['rates']
+    else:
+        return None
