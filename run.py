3e9f87f01ecd0e4632f9764b4edd7a67dfa43451
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-06-27 04:16:24 +0000

    Add function to retrieve exchange rate from googlesheet and use the API echangerate.host as a backup, and edit comments accordingly

--- run.py
+++ run.py
@@ -1,26 +1,36 @@
+# Import the necessary modules
 import requests
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Define the scope for Google Sheets API
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+# Load credentials from the 'creds.json' file and create a client to interact with Google Sheets
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('exchange_currency')
 
+# Access the 'codes' worksheet from the sheet
+WORKSHEET = SHEET.worksheet("codes")
 
+# Function to retrieve exchange rate from Google Sheets
+def get_rate_from_sheet(base_currency, target_currency):
+    all_records = WORKSHEET.get_all_records()
+    for record in all_records:
+        # If a record matches the base and target currency, return the exchange rate
+        if record['code'] == base_currency and record['code'] == target_currency:
+            return record['exchange_rate']
+    return None
+
+# Function to fetch exchange rates using the 'exchangerate.host' API
 def get_all_exchange_rates(base_currency):
-    """
-    Fetches the latest exchange rates for the given base currency and
-    returns a dictionary of currency codes and corresponding rates.
-    """
     url = f'https://api.exchangerate.host/latest?base={base_currency}'
-
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -30,76 +40,61 @@ def get_all_exchange_rates(base_currency):
         print(f"Error fetching exchange rates: {e}")
         return None
 
-
+# Function to validate a currency code
 def is_valid_currency_code(code):
-    """
-    Checks if the input string is a valid 3 letter currency code.
-    """
     return len(code) == 3 and code.isalpha()
 
-
+# Function to validate the conversion amount
 def is_valid_amount(amount):
-    """
-    Checks if the input string is a valid positive number.
-    """
     try:
         return float(amount) > 0
     except ValueError:
         return False
 
-
+# Main function where the conversion happens
 def main():
-
     print("Welcome to Troca-Currency Converter!")
-    print("Troca-Currency Converter is a straightforward tool for converting currencies.")
-    print("Enter the base currency, the target currency, and the conversion amount.")
-    print("Follow the instructions to complete the conversion.\n")
     while True:
-        # Input Source Currency
         base_currency = input("Enter the base currency (e.g. USD): ").upper()
         if not is_valid_currency_code(base_currency):
             print("Please enter a valid 3-letter currency code.")
             continue
 
-
-        # Looking for conversion rates for a base currency
-        exchange_rates = get_all_exchange_rates(base_currency)
-        if not exchange_rates:
-            continue
-
-
-        # Input Target Currency
         target_currency = input("Enter the target currency to convert to (e.g. EUR): ").upper()
         if not is_valid_currency_code(target_currency):
             print("Please enter a valid 3-letter currency code.")
             continue
 
-
-        # Input Amount
         amount = input("Enter the amount to be converted: ")
         if not is_valid_amount(amount):
             print("Please enter a valid positive number.")
             continue
-        
+
         amount = float(amount)
 
+        # Try to get the rate from the Google Sheet first
+        rate = get_rate_from_sheet(base_currency, target_currency)
 
-        # Calculate Conversion
-        rate = exchange_rates.get(target_currency)
+        if rate is None:
+            # If the rate is not in the sheet, use the API
+            exchange_rates = get_all_exchange_rates(base_currency)
+            if exchange_rates:
+                rate = exchange_rates.get(target_currency)
+
         if rate:
+            # If the rate is obtained, perform the conversion
             converted_amount = rate * amount
             print(f"{amount} {base_currency} is equal to {converted_amount:.2f} {target_currency}.")
         else:
             print(f"Exchange rate for {target_currency} not available.")
 
-
-        # if user wants to do another conversion
+        # Ask the user if they want to make another conversion
         another_conversion = input("Do you want to make another conversion? (Y/N): ").upper()
         if another_conversion != 'Y':
             break
 
-
 if __name__ == "__main__":
-    # Start
     main()
+
+# End message after the conversions
 print("Thank you for using Troca-Currency Converter!")
